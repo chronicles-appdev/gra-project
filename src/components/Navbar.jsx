c3ffdae38e444ffd07c117fef0a23fc4a2674aaf
chiomavera <you@example.com>
2024-03-06 15:49:37 +0100

    make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,83 @@
-import { Link } from "react-router-dom"
-import logo from "/logo.png"
+import { Link } from "react-router-dom";
+import logo from "/logo.png";
+import { useState, useEffect } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-  return (
-    <header className="bg-cream text-gray fixed z-10 top-0 w-full font-semibold flex justify-between items-center p-4 ">
-        <div>
-            <img src={logo} alt="" className="w-14 h-14" />
-        </div>
-        <div className="flex justify-between gap-20">
-        <Link to="/" className="hover:border-b-[2px] border-gray pb-2">Home</Link>
-        <Link to="/about" className="hover:border-b-[2px] border-gray pb-2">About</Link>
-        <Link to="/services" className="hover:border-b-[2px] border-gray pb-2">Services</Link>
-        <Link to="/contact-us" className="hover:border-b-[2px] border-gray pb-2">Contact Us</Link>  
-        </div>
-      
-    </header>
-  )
-}
+  const [open, setOpen] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+
+  const closeMenu = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const thresholdWidth = 768;
+      if (window.innerWidth >= thresholdWidth) {
+        setOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const links = [
+    { to: "/", label: "Home" },
+    { to: "/about", label: "About" },
+    { to: "/services", label: "Services" },
+    { to: "/contact-us", label: "Contact Us" },
+  ];
+
+  return (
+    <header className="bg-cream text-gray fixed z-10 top-0 w-full font-semibold">
+      <nav className="flex justify-between items-center py-4 px-6">
+        <img src={logo} alt="" className="w-14 h-14" />
+        <div
+          onClick={toggleMenu}
+          className="text-3xl absolute right-3 lg:hidden cursor-pointer"
+        >
+          {open ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </div>
+        <div
+          className={`lg:hidden absolute top-full left-0 right-0 bg-cream z-20 ${
+            open ? "block" : "hidden"
+          }`}
+        >
+          <div className="flex flex-col items-center">
+            {links.map((link, index) => (
+              <Link
+                key={index}
+                to={link.to}
+                className="border-b border-gray pb-2 w-full text-center"
+                onClick={closeMenu}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="hidden lg:flex items-center justify-between gap-10">
+          {links.map((link, index) => (
+            <Link
+              key={index}
+              to={link.to}
+              className="hover:border-b-[2px] border-gray pb-2"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
