adc396af6073e01a4e273c200c2db2e966969d33
Mistie-rious <misturayahaya1@gmail.com>
2024-05-29 14:11:32 +0100

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Root from "./routes/root";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -6,24 +6,18 @@ import Contact from "./pages/ContactPage";
 import Services from "./pages/services";
 
 const App = () => {
-
-  const router = createBrowserRouter (
-    createRoutesFromElements(
-     <Route path="/" element={<Root />}>
-      <Route index element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact-us" element={<Contact />} />
-      <Route path="/services" element={<Services />} />
-     </Route>
-    
-    )
-  )
-  
   return (
-    <>
-     <RouterProvider router={router}/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
